a2ec0f32db649e5e78f62a0b27e05c05516735e3
InderpreetKalsi <ikalsi@delaplex.com>
2023-09-27 12:35:20 +0530

    first db project

---
+++ mfg_db_dev/mfgTable1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[mfgTable1]
+(
+	[Id] INT NOT NULL , 
+    [Name] VARCHAR(50) NULL, 
+    [Gender] NCHAR(10) NULL, 
+    CONSTRAINT [PK_mfgTable1_Id] PRIMARY KEY ([Id])
+)
---
+++ mfg_db_dev/sql_scripts/2023-09-26.sql
@@ -0,0 +1,19 @@
+﻿-- delete from mp_rating_responses where to_company_id  = 1837822 and from_id = 1346595
+-- update mp_star_rating set no_of_stars = 5	, total_responses = 2 where company_id = 1837822
+
+select * from mp_star_rating  where company_id = 1837822
+select * from mp_rating_responses where to_company_id  = 1837822  and from_id = 1346595
+
+
+
+select * from mp_contacts where contact_id in (1367274)
+
+--1367274
+
+-- rc : 17889
+select company_id ,name ,Assigned_SourcingAdvisor  
+from mp_companies 
+where company_id in (select company_id from mp_contacts where is_buyer = 1) and Assigned_SourcingAdvisor = 1345421
+
+update mp_companies set  Assigned_SourcingAdvisor = 1367274
+where company_id in (select company_id from mp_contacts where is_buyer = 1) and Assigned_SourcingAdvisor = 1345421
